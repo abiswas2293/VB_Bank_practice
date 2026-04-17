74ef703d7793b4516561f28be079e94352bb0004
ann <abiswas2293@yahoo.com>
2026-04-17 09:14:14 +0100

    added-homepage-Locators

---
+++ src/main/java/ObjectRepository/homePageLocators.java
@@ -0,0 +1,12 @@
+package ObjectRepository;
+
+public class homePageLocators {
+   public  String homePageTitle="";
+    public String headers(){
+        return "";
+    }
+    public String liveCurrency(){
+        return "";
+    }
+    public String accountNumber="";
+}
