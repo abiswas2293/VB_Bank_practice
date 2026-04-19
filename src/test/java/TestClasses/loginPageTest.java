0ecc9c2468d7534344c849db7944f601c5fc69ad
ann <abiswas2293@yahoo.com>
2026-04-19 09:31:55 +0100

    added-login-page-test

---
+++ src/test/java/TestClasses/loginPageTest.java
@@ -0,0 +1,36 @@
+package TestClasses;
+
+import PageClasses.loginPage;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import utility.Baseclass;
+
+public class loginPageTest {
+    Baseclass bc = new Baseclass();
+    loginPage lp = new loginPage();
+
+    @BeforeTest
+    public void before() {
+        bc.openBrowser();
+    }
+
+    @AfterTest
+    public void after() {
+        bc.closeBrowser();
+    }
+
+    @Test
+    public void verifyAllElementsOnLoginPage(){
+        lp.verifyLoginPageTitle();
+        lp.verifyUserNameText();
+        lp.verifyUserNameText();
+        lp.verifyPasswordText();
+        lp.verifyPasswordTextboxIsEnabled();
+        lp.verifyLoginAsUserButton();
+        lp.verifyLoginAsAdmin();
+        lp.verifyLoginButton();
+        lp.verifyRegisterHereButtonAndClickOnToRegister();
+        lp.verifyAfterClickingOnRegisterHereButtonOnLoginPageUserIsOnRegistrationPage();
+    }
+}
