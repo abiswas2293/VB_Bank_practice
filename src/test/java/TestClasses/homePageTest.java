1c60f51d71c6bd5a74099a8cab7bbdf50f4148ee
ann <abiswas2293@yahoo.com>
2026-04-22 10:20:37 +0100

    added code for homepage test

--- src/test/java/TestClasses/homePageTest.java
+++ src/test/java/TestClasses/homePageTest.java
@@ -5,9 +5,9 @@ import org.testng.annotations.*;
 import utility.Baseclass;
 
 public class homePageTest {
-    Baseclass bc=new Baseclass();
-    registrationPageTest rpt=new registrationPageTest();
-    homePage hp=new homePage();
+    Baseclass bc = new Baseclass();
+    registrationPageTest rpt = new registrationPageTest();
+    homePage hp = new homePage();
 
     @BeforeClass
     @Parameters({"browser"})
@@ -21,26 +21,25 @@ public class homePageTest {
     }
 
     @Test
-    public void verifyAllElementsArePresentOnHomePageAndVerifyAccountNumber(){
-        rpt.verifyAfterFillingOutRegistrationPageUserIsSuccessfullyRegisteredAndRedirectToHomePage();
+    public void verifyAllElementsArePresentOnHomePageAndVerifyAccountNumber() {
+        rpt.verifyAfterFillingOutRegistrationPageUserIsSuccessfullyRegisteredAndRedirectToHomePage("", "", "", "");
         hp.verifyAllSidePanelsArePresent();
         hp.verifyAllLiveCurrency();
     }
 
-    @Test
-    public void verifyAfterClickingOnTopUpUserIsOnTopUpPage(){
-        rpt.verifyAfterFillingOutRegistrationPageUserIsSuccessfullyRegisteredAndRedirectToHomePage();
+
+    public void verifyAfterClickingOnTopUpUserIsOnTopUpPage() {
+        rpt.verifyAfterFillingOutRegistrationPageUserIsSuccessfullyRegisteredAndRedirectToHomePage("", "", "", "");
         hp.clickTopUp();
         hp.verifyTopUpPageUrl();
     }
 
-    @Test
-    public void verifyAfterClickingOnBillPaymentUserIsOnBillPaymentPage(){
-        rpt.verifyAfterFillingOutRegistrationPageUserIsSuccessfullyRegisteredAndRedirectToHomePage();
+
+    public void verifyAfterClickingOnBillPaymentUserIsOnBillPaymentPage() {
+        rpt.verifyAfterFillingOutRegistrationPageUserIsSuccessfullyRegisteredAndRedirectToHomePage("", "", "", "");
         hp.clickBillPayment();
         hp.verifyBillPaymentPageUrl();
     }
-
 
 
 }
